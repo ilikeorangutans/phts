7f1d4cb331f1518214ab3bf6793bb3740f0a69cd
Jakob Külzer <jakob.kuelzer@gmail.com>
2017-10-08 13:04:08 -0400

    made upload a bit better

--- frontend/admin/src/app/photos/photo-upload/photo-upload.component.ts
+++ frontend/admin/src/app/photos/photo-upload/photo-upload.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { PhotoService } from "../../services/photo.service";
-import { Collection } from "../../models/collection";
+import { PhotoService } from '../../services/photo.service';
+import { Collection } from '../../models/collection';
 
 @Component({
   selector: 'app-photo-upload',
@@ -11,6 +11,10 @@ export class PhotoUploadComponent implements OnInit {
 
   @Input() collection: Collection;
 
+  dropFilesMessage = 'Drop files here...';
+
+  message = this.dropFilesMessage;
+
   constructor(
     private photoService: PhotoService
   ) { }
@@ -18,27 +22,52 @@ export class PhotoUploadComponent implements OnInit {
   ngOnInit() {
   }
 
+  getFiles(event: DragEvent): Array<File> {
+    const result = Array<File>();
+    const dt = event.dataTransfer;
+    if (dt.items) {
+      console.log('using data transfer item list');
+      for (let i = 0; i < dt.items.length; i++) {
+        const f = dt.items[i].getAsFile();
+        console.log('got file', f);
+
+        result.push(f);
+      }
+    } else {
+      console.log('using data transfer');
+      // TODO
+    }
+
+    return result;
+  }
+
+  onDragLeave(): Boolean {
+    this.message = this.dropFilesMessage;
+    return false;
+  }
   onDrop(event: DragEvent): Boolean {
     event.stopPropagation();
     event.preventDefault();
-    console.log("onDrop()");
+    console.log('onDrop()');
 
-    let dt = event.dataTransfer;
+    const dt = event.dataTransfer;
     if (dt.items) {
-      console.log("using data transfer item list")
+      console.log('using data transfer item list');
       for (let i = 0; i < dt.items.length; i++) {
-        let f = dt.items[i].getAsFile();
-        console.log("got file", f);
+        const f = dt.items[i].getAsFile();
+        console.log('got file', f);
 
         // TODO would be cool if we had a queue
         this.photoService.upload(this.collection, f);
       }
     } else {
-      console.log("using data transfer")
+      console.log('using data transfer');
       // TODO
     }
 
 
+
+    this.message = this.dropFilesMessage;
 
     return false;
   }
@@ -47,11 +76,14 @@ export class PhotoUploadComponent implements OnInit {
     event.stopPropagation();
     event.preventDefault();
 
+    const files = this.getFiles(event);
+    this.message = `Upload ${files.length} files...`;
+
     return false;
   }
 
   onDragEnd(event: DragEvent): Boolean {
-    console.log("onDragEnd()");
+    console.log('onDragEnd()');
     return false;
   }
 }
