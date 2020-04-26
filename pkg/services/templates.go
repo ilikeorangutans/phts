10ea17cf345bc2ade40470a6138c3c40a6117e42
Jakob Külzer <jakob.kuelzer@gmail.com>
2020-04-25 21:43:41 -0400

    Cleaning up UI a bit

--- pkg/services/templates.go
+++ pkg/services/templates.go
@@ -6,6 +6,8 @@ var BaseTmpl = template.Must(template.ParseFiles("pkg/services/base.tmpl"))
 
 var LoginPageTmpl = template.Must(template.Must(BaseTmpl.Clone()).ParseFiles("pkg/services/login_page.tmpl"))
 
-var LandingPageTmpl = template.Must(template.Must(BaseTmpl.Clone()).ParseFiles("pkg/services/landing_page.tmpl"))
+var BaseUITmpl = template.Must(template.Must(BaseTmpl.Clone()).ParseFiles("pkg/services/base_ui.tmpl"))
 
-var ServiceUsersPageTmpl = template.Must(template.Must(BaseTmpl.Clone()).ParseFiles("pkg/services/service_users_page.tmpl"))
+var LandingPageTmpl = template.Must(template.Must(BaseUITmpl.Clone()).ParseFiles("pkg/services/landing_page.tmpl"))
+
+var ServiceUsersPageTmpl = template.Must(template.Must(BaseUITmpl.Clone()).ParseFiles("pkg/services/service_users_page.tmpl"))
