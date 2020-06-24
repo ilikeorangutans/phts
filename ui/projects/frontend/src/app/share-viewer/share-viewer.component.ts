9e7f604f3a0778df829c7c3421c939fa998933dc
Jakob Külzer <jakob.kuelzer@gmail.com>
2020-06-23 23:27:55 -0400

    automatically select best fit rendition when viewing shares

--- ui/projects/frontend/src/app/share-viewer/share-viewer.component.ts
+++ ui/projects/frontend/src/app/share-viewer/share-viewer.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { ShareService } from '../services/share.service';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { map, switchMap } from 'rxjs/operators';
+import { map, switchMap, tap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+
+import { ShareService } from '../services/share.service';
 import { Share } from '../models/share';
 
 @Component({
@@ -11,7 +12,11 @@ import { Share } from '../models/share';
   styleUrls: ['./share-viewer.component.css'],
 })
 export class ShareViewerComponent implements OnInit {
-  constructor(private shares: ShareService, private route: ActivatedRoute) {}
+  constructor(private shares: ShareService, private route: ActivatedRoute) { }
+
+  screenWidth: number;
+  screenHeight: number;
+  photoIndex = 0;
 
   share$: Observable<Share>;
 
@@ -20,9 +25,22 @@ export class ShareViewerComponent implements OnInit {
   ngOnInit(): void {
     this.share$ = this.route.paramMap.pipe(
       map((params) => params.get('slug') as string),
-      switchMap((slug) => this.shares.forSlug(slug))
+      switchMap((slug) => this.shares.forSlug(slug)),
+      tap(share => this.findBestFit(share))
     );
-    // TODO here we should get the index of the current photo from the param map
+  }
+
+  findBestFit(share: Share) {
+    const screenArea = window.innerHeight * window.innerWidth;
+    const photo = share.photos[this.photoIndex];
+    const bestFit = photo.renditions.sort((a, b) => Math.abs(screenArea - (a.width * a.height)) - Math.abs(screenArea - (b.width * b.height)))[0];
+    this.renditionIndex = photo.renditions.findIndex((rendition, _) => rendition.id === bestFit?.id);
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onScreenSizeChange(_) {
+    this.screenHeight = window.innerHeight;
+    this.screenWidth = window.innerWidth;
   }
 
   switchRendition(index: number) {
